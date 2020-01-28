35d78e9fb534f37c7862ed07c1c77d4670d48fb7
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2020-01-28 09:53:04 +0000

    0.1.3 - Basic billboard
    I have implemented a basic billboard system which means that the cryptid in the scene will turn to look at the player

---
+++ Shutterbug/Assets/Scripts/CryptidBehaviour/BilboardRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BilboardRotation : MonoBehaviour
+{
+    private Transform m_transform;
+
+    private GameObject m_goPlayer;
+
+    private void Start()
+    {
+        m_transform = transform;
+        m_goPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.LookAt(m_goPlayer.transform);
+    }
+}
