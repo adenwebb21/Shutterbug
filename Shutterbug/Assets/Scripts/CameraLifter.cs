0341e99243a961fdc20f904dca17e15744db752f
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2020-01-28 15:24:28 +0000

    0.1.8 - Added better camera transitions
    The camera now has a render texture which is brought up to the players face

---
+++ Shutterbug/Assets/Scripts/CameraLifter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLifter : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<Animator>().SetTrigger("LiftCamera");
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            this.GetComponent<Animator>().SetTrigger("DropCamera");
+        }
+    }
+}
