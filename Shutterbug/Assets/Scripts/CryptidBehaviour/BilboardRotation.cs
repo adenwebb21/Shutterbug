ff2f52a45d22b2739da20a778a4c404ae28b215b
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2020-01-28 10:21:56 +0000

    0.1.4 - Advanced billboard
    I have implemented the more advanced billboarding, where the cryptid hasa front and a back - it can rotate within these 180 degree areas.

--- Shutterbug/Assets/Scripts/CryptidBehaviour/BilboardRotation.cs
+++ Shutterbug/Assets/Scripts/CryptidBehaviour/BilboardRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BilboardRotation : MonoBehaviour
 {
+    private Transform m_parentTransform;
     private Transform m_transform;
 
     private GameObject m_goPlayer;
@@ -11,11 +12,26 @@ public class BilboardRotation : MonoBehaviour
     private void Start()
     {
         m_transform = transform;
+        m_parentTransform = m_transform.parent.transform;      
         m_goPlayer = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
-        transform.LookAt(m_goPlayer.transform);
+        Vector3 _vectorToTarget = (m_goPlayer.transform.position - m_parentTransform.position).normalized;
+
+        if (Vector3.Dot(_vectorToTarget, m_parentTransform.forward) > 0)
+        {
+            //Debug.Log("Player is in front of this game object.");
+            m_transform.rotation = Quaternion.LookRotation(m_goPlayer.transform.position - m_parentTransform.position);
+
+        }
+        else
+        {
+            //Debug.Log("Player is not in front of this game object.");
+            m_transform.rotation = Quaternion.LookRotation(m_transform.position - m_goPlayer.transform.position);
+        }
+
+        //transform.LookAt(m_goPlayer.transform);
     }
 }
