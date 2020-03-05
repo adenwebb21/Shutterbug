3cd8ba4df0734098ff22e841b6e3fa01420759e1
Aden <adenwebb21@gmail.com>
2020-03-05 14:58:14 +0000

    0.3.17 - Small tweaks
    I have made the spawn points work better and also not include the initial spawn poiints

--- Shutterbug/Assets/Scripts/InitialCryptidSpawn.cs
+++ Shutterbug/Assets/Scripts/InitialCryptidSpawn.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class InitialCryptidSpawn : MonoBehaviour
 {
-    Transform _firstSpawn;
+    public Transform _firstSpawn;
 
     void Start()
     {
-        _firstSpawn = transform.GetChild(0);
         GameManager.Instance.currentCryptid = Instantiate(GameManager.Instance.cryptidPrefab, _firstSpawn.position, _firstSpawn.rotation);
     }
 }
