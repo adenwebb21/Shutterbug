3170b3fee873a8c410e1ae784010b3e2df49cbf1
Aden Webb <adenwebb21@gmail.com>
2020-04-05 16:58:25 +0100

    0.5.8 - Handbook now scales correctly when it opens and closes
    The handbook now scales properly when it opens and closes. The player can also choose when to start the game!

---
+++ Shutterbug/Assets/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Animator book, frontCover, firstPage;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OpenBook();
+        }
+    }
+
+    private void OpenBook()
+    {
+        book.Play("move_book_offcentre");
+        frontCover.Play("front_dissapear");
+    }
+}
